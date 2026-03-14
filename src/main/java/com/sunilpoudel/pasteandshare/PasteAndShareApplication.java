2a48f57a96a4a40c9fee9ed31de922f3ff875491
Sunil Poudel <suneelpdl90@gmail.com>
2026-03-14 13:18:50 +0545

    initial commit

---
+++ src/main/java/com/sunilpoudel/pasteandshare/PasteAndShareApplication.java
@@ -0,0 +1,13 @@
+package com.sunilpoudel.pasteandshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteAndShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasteAndShareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sunilpoudel/pasteandshare/PasteAndShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunilpoudel.pasteandshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteAndShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
